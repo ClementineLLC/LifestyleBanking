ba1eded5ee531a83540e32da0aa37692142a3090
Aditi Chakravarthi <aditi@one45.com>
2015-12-02 00:15:11 -0800

    Modified existing code to get basic structure of goals page.

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,24 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
+    name: 'Trip to Bora Bora',
+    lastText: '$1570/$5000',
     face: 'img/ben.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
+    name: 'Boyfriend\'s birthday gift',
+    lastText: '$10/$150',
     face: 'img/max.png'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
+    name: 'Yoga Retreat',
+    lastText: '$380/$400',
     face: 'img/adam.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
+    name: 'Grouse Grind Gondola Pass',
+    lastText: '$120/$120',
     face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
   }];
 
   return {
